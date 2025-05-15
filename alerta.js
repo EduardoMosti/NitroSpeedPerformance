031e5159d74b64c8c98545bf908c1c18db7c0ddc
Eduardo Mosti <eduardo.mosti1@gmail.com>
2025-05-14 22:08:39 -0300

    Primeiro Commit

---
+++ alerta.js
@@ -0,0 +1,4 @@
+alert('Seja bem vindo a nossa pagina de produtos disponíveis!')
+dia = new Array ("Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado")
+mes = new Array ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
+data = new Date
